5691797e9109ef997c33ba6aca8824366f0b177c
chenzelin <chenzelinmail@gmail.com>
2017-05-26 09:35:47 +0800

    Signed-off-by: chenzelin <chenzelinmail@gmail.com>

--- PresentResult.py
+++ PresentResult.py
@@ -2,19 +2,70 @@
 import jieba
 from QAModel import QAModel
 from DBModel import DBModel
-if __name__=='__main__':
-    q = "我很好奇dolls特刑部队的队长是谁？"
-    ans = ["法务省 特别死刑执行刑务官部 第一部队 总队长: 御子柴笑太,副部长:式部清寿, 队员：藤堂羽沙希",
-         "由负责故事设计的咲木音穂（saki otoo，12月6日）以及负责绘画的中村友美（nakamura tomomi，3月17日）两人组成）连载。",
-         "这个人身上充满了复杂的矛盾感，却不会显得不协调。"]
+import numpy as np
 
-    # q = list(jieba.cut(q))
-    # e = list(jieba.cut(e))
-    db_model = DBModel()
-    db_model.load_model('db_model.h5')
+def find_index(list, word):
+     try:
+        return list.index(word)
+     except:
+         return -1
+
+
+if __name__=='__main__':
+    q = "中西区圣安多尼学校是什么时候成立了校友会？"
+    q = list(jieba.cut(q))
+    ans = ["中西区圣安多尼学校（英文：Central And Western District Saint Anthony's School）于1963年创立，隶属鲍思高慈幼会中华会省。",
+           "中西区圣安多尼学校是2005年9月17日成立校友会。",
+        "2006年9月:完成所有收生程序，正式成为一所男女校。",
+        "盾上方写着“中西区圣安多尼学校”的中英文字样，下方中央有一特式百合花，两旁有代表校名的英文简写\"CWSA\"，以及中文校训：“笃信博爱”。"]
     qs = []
-    es = []
-    for e in ans:
-        qs.append(q)
-    result = db_model.predict_by_sentences(qs, ans)
+    qes = []
+    for i in range(len(ans)):
+        ans[i] = list(jieba.cut(ans[i]))
+    es = ans
+    max_qe_ee_len = 100
+    qe = np.zeros(max_qe_ee_len)
+    for q_word_index in range(len(q)):
+        q_word = q[q_word_index]
+        for a in ans:
+            if find_index(a, q_word) is not -1:
+                qe[q_word_index] = 1
+                break
+
+    ans_len = len(ans)
+    ees = np.zeros((ans_len, max_qe_ee_len))
+
+    index_i = 0
+    while index_i < ans_len:
+        for ans_word_index in range(len(ans[index_i])):
+            ans_word = ans[index_i][ans_word_index]
+            for index_j in range(index_i+1, ans_len):
+                word_find_index = find_index(ans[index_j], ans_word)
+                if word_find_index is not -1:
+                    # the vector of finding answer
+                    ees[index_i][ans_word_index] = 1
+                    # the vector of where mactch answer
+                    ees[index_j][word_find_index] = 1
+        index_i += 1
+        qes.append(qe)
+
+    qa = QAModel()
+    input_qs = []
+    input_es = []
+    input_qes = []
+    input_ees = []
+    for input_e, input_qe, input_ee in zip(es, qes, ees):
+        input_q, input_e, input_qe, input_ee = qa.get_input_q_e_g1_g2(q, input_e, input_qe, input_ee)
+        input_qs.append(input_q)
+        input_es.append(input_e)
+        input_qes.append(input_qe)
+        input_ees.append(input_ee)
+
+    input_qs = np.array(input_qs)
+    input_es = np.array(input_es)
+    input_qes = np.array(input_qes)
+    input_ees = np.array(input_ees)
+    qa.load_model('qamodel_0.1.h5')
+    result = qa.predict(input_qs, input_es, input_qes, input_ees)
     print(result)
+
--- QAModel2.py
+++ QAModel2.py
@@ -10,7 +10,7 @@ import numpy as np
 from evaluation import raw_result_parser
 from evaluation import tagging_util
 
-class QAModel:
+class QAModel2:
     def __init__(self):
         self.wordModel = Word2Vec.load('model_new_and_wiki')
         self.word_dim = 100
@@ -32,37 +32,32 @@ class QAModel:
         # question_x = Embedding(output_dim=question_len,
         #               input_dim=word_dim,
         #               input_length=question_len)(question_input)
-        q = LSTM(D, dropout=dropout_rate, return_sequences=True)(question_input)
+        q = LSTM(self.question_len, dropout=dropout_rate, return_sequences=True, activation='sigmoid')(question_input)
 
         alpha_tanh = Dense(self.question_len, activation='tanh')(q)
         alpha = Dense(1, activation='softmax')(alpha_tanh)
         alpha_multiply = Multiply()([q, alpha])
         question_represent = Lambda(lambda x: K.sum(x, axis=1), name='question_representation')(alpha_multiply)
-        # g1 layer
-        qe_input = Input(shape=(self.evidence_len, 1), name='qe')
-        # qe_layer = TimeDistributed(Embedding(input_dim=1, output_dim=1, name='qe_layer'))(qe_input)
-        ee_input = Input(shape=(self.evidence_len, 1), name='ee')
-        # ee_layer = TimeDistributed(Embedding(input_dim=1, output_dim=1, name='ee_layer'))(ee_input)
         # evidence layer
         evidence_input = Input(shape=(self.evidence_len, self.word_dim), name='e_input', dtype=np.float32)
         # what the fuck is g1 and g2 in the paper formula  10, don't care, ignore first
         question_represent_repeat = RepeatVector(self.evidence_len)(question_represent)
-        evidence_x = concatenate([evidence_input, question_represent_repeat, qe_input, ee_input], name='input_e')
+        evidence_x = concatenate([evidence_input, question_represent_repeat], name='input_e')
         # evidence_x = Embedding(output_dim=evidence_len,
         #                        input_dim=word_dim + 1,
         #                        input_length=evidence_len)(evidence)
-        lstm_1 = LSTM(64, dropout=dropout_rate, return_sequences=True)(evidence_x)
-        lstm_2 = LSTM(64, dropout=dropout_rate, return_sequences=True)(lstm_1)
+        lstm_1 = LSTM(64, dropout=dropout_rate, return_sequences=True, activation='sigmoid')(evidence_x)
+        lstm_2 = LSTM(64, dropout=dropout_rate, return_sequences=True, activation='sigmoid')(lstm_1)
         lstm_2_reverse = Lambda(lambda x: K.reverse(x, 0), name='lstm_reverse')(lstm_2)
-        lstm_3 = LSTM(self.evidence_len, dropout=dropout_rate, return_sequences=True)(concatenate([lstm_1, lstm_2_reverse]))
+        lstm_3 = LSTM(self.evidence_len, dropout=dropout_rate, return_sequences=True, activation='sigmoid')(concatenate([lstm_1, lstm_2_reverse]))
         # CRF layer
         tp_layer = TimeDistributed(Dense(4))(lstm_3)
         crf_layer = crf.ChainCRF(name='crf_layer')
         crf_o = crf_layer(tp_layer)
 
-        model = Model(inputs=[question_input, evidence_input, qe_input, ee_input], outputs=[crf_o])
+        model = Model(inputs=[question_input, evidence_input], outputs=[crf_o])
         model.summary()
-        model.compile(optimizer='rmsprop', loss=crf_layer.loss, loss_weights=[0.01])
+        model.compile(optimizer='rmsprop', loss=crf_layer.loss, loss_weights=[0.001])
         self.model = model
         return self.model
 
@@ -73,35 +68,30 @@ class QAModel:
         for q_token, e_token, golden_label, g1, g2 in data:
             output_seq = np.zeros((self.evidence_len, 3))
             output_seq = np.hstack((output_seq, np.ones((self.evidence_len, 1))))
-            input_q, input_e, input_qe, input_ee = self.get_input_q_e_g1_g2(q_token, e_token, g1, g2)
+            input_q, input_e= self.get_input_q_e(q_token, e_token)
             for label_index in range(min(self.evidence_len, len(golden_label))):
                 output_seq[label_index] = golden_label[label_index]
-            yield input_q, input_e, input_qe, input_ee, output_seq
+            yield input_q, input_e, output_seq
             load_cur += 1
             if load_cur > load_max:
                 break
 
     ''' @:param the list of split question and evidence
         @:return a maxtric which col is the word_vec of the split word'''
-    def get_input_q_e_g1_g2(self, question, evidence, g1, g2):
+    def get_input_q_e(self, question, evidence):
         input_q = np.zeros((self.question_len, self.word_dim))
-        input_g1 = np.zeros((self.evidence_len, 1))
         input_e = np.ones((self.evidence_len, self.word_dim))
-        input_g2 = np.ones((self.evidence_len, 1))
         for word_index in range(min(self.question_len, len(question))):
             word = question[word_index]
             input_q[word_index] = self.word_vec(word)
         for word_index in range(min(self.evidence_len, len(evidence))):
             word = evidence[word_index]
             input_e[word_index] = self.word_vec(word)
-        for g1_index in range(min(self.evidence_len, len(g1))):
-            input_e[g1_index] = g1[g1_index]
-        for g2_index in range(min(self.evidence_len, len(g2))):
-            input_e[g2_index] = g2[g2_index]
-        return input_q, input_e, input_g1, input_g2
 
-    def fit(self, input_qs, input_es, input_qe, input_ee, train_labels):
-        self.model.fit([input_qs, input_es, input_qe, input_ee], [train_labels], epochs=10, batch_size=120)
+        return input_q, input_e
+
+    def fit(self, input_qs, input_es, train_labels):
+        self.model.fit([input_qs, input_es], [train_labels], epochs=10, batch_size=120)
 
     def save_model(self, model_file):
         self.model.save_weights(model_file)
@@ -112,18 +102,15 @@ class QAModel:
         return self.model
 
     ''':param input_qes and input_ees should be type() as np.array'''
-    def predict(self, input_qs, input_es, input_qes, input_ees):
+    def predict(self, input_qs, input_es):
         if type(input_qs) is list:
             input_qs = np.array(input_qs)
         if type(input_es) is list:
             input_es = np.array(input_es)
-        return self.model.predict([input_qs, input_es, input_qes, input_ees])
+        return self.model.predict([input_qs, input_es])
 
 if __name__ == '__main__':
-    config = tf.ConfigProto()
-    config.gpu_options.per_process_gpu_memory_fraction = 0.3
-    set_session(tf.Session(config=config))
-    qa = QAModel()
+    qa = QAModel2()
     qa.get_model()
     # for test_q, test_e, test_labels in qa.load_data('WebQA.v1.0/data/test.ir.json.gz'):
     #     print(test_q, test_e, test_labels)
@@ -131,23 +118,17 @@ if __name__ == '__main__':
     iters = qa.load_train_data('WebQA.v1.0/data/training.json.gz')
     train_qs = []
     train_es = []
-    train_qes = []
-    train_ees = []
     train_labels = []
-    for train_q, train_e, train_qe, train_ee, train_label in iters:
+    for train_q, train_e, train_label in iters:
         train_qs.append(train_q)
         train_es.append(train_e)
-        train_qes.append(train_qe)
-        train_ees.append(train_ee)
         train_labels.append(train_label)
 
     train_qs = np.array(train_qs, dtype=np.float32)
     train_es = np.array(train_es, dtype=np.float32)
-    train_qes = np.array(train_qes, dtype=np.float32)
-    train_ees = np.array(train_ees, dtype=np.float32)
     train_labels = np.array(train_labels, dtype=np.int32)
 
-    qa.fit(train_qs, train_es, train_qes, train_ees, train_labels)
+    qa.fit(train_qs, train_es, train_labels)
     qa.save_model('qamodel_save_test.h5')
     # qa.load_model('qamodel_save_test.h5')
     # qamodel.save_weights('qamodel.h5')
